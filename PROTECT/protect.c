bd827512e00b8ea47cdae33deda72ddb1b34b81d
Aspopnly <2756283851@qq.com>
2023-08-04 18:01:13 +0800

    fix_protect

--- PROTECT/protect.c
+++ PROTECT/protect.c
@@ -4,11 +4,12 @@
 
 #define MAX_SAFE_CURRENT 8
 extern float32_t ADC1_Buf_f32[4];
+extern float32_t ADC3_Buf_f32[4];
 
 
 void PROTECT_GUARD(void)
 {
-    if(ADC1_Buf_f32[1]>MAX_SAFE_CURRENT) 
+    if(ADC3_Buf_f32[2]>MAX_SAFE_CURRENT) 
     {
         HOLD_MOS();
         while (1)
